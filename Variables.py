293064a1e6c1e9bf511da8abaccaf1198fdb806c
DinuthInduwara <89961332+DinuthInduwara@users.noreply.github.com>
2021-11-01 20:55:05 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,109 @@
+from telegram.ext import *
+from telegram import *
+
+
+
+Photos = "1.All Officialy Relesed Photos"
+Interviews = "2.Emma Watson Interviews"
+Tiktok_And_Others = "3.TikTok And Other Short Videos"
+Movies = "4.Emma Watson Acted Movies"
+Random_Photos_And_Videos = "5.Random HighQuality Photos And Videos"
+Watch_And_Download_All = "6.Watch All And Download"
+
+
+
+හයකටකලින්එක = "Emma Watson launches  next phase of the HeForShe at World Economic Forum in Davos" #https://www.youtube.com/watch?v=0IS-9tI7-2o
+හයකටකලින්දෙක = "Emma Watson Facebook Q&A about HeForShe - International Women's Day 2015"  #https://www.youtube.com/watch?v=n4xzvDzP-lA
+හයකටකලින්තුන = "Regression Official Featurette #1 Emma Watson, Alejandro Amenábar"   #https://www.youtube.com/watch?v=s5LqlhRbQV4
+පහකටකලින්එක = "Emma Watson interviews Malala Yousafzai Nobel Peace Prize"  #https://www.youtube.com/watch?v=NKckKStggSY
+පහකටකලින්දෙක = "Emma Watson Backstage Vogue Italia 2015"  #https://www.youtube.com/watch?v=7AzSpttmlfI
+පහකටකලින්තුන = "Emma Watson Introduces the new HeForShe"  #https://www.youtube.com/watch?v=u6iJN8OovWg
+පහකටකලින්හතර = "Emma Watson interview in Scoop With Raya"  #https://www.youtube.com/watch?v=LKerYFqqmpM&t=7s
+පහකටකලින්පහ = "Emma Watson interviews Lin-Manuel Miranda for HeForShe Arts Week"  #https://www.youtube.com/watch?v=-NbEbkVrVWY
+පහකටකලින්හය = "Emma Watson - HITRECORD, Technology is like a Superpower"   #https://www.youtube.com/watch?v=1jxAsYdxhqw
+පහකටකලින්හත = "Emma Watson interview for Colonia"  #https://www.youtube.com/watch?v=OBywDqT5PeY
+පහකටකලින්අට =  "Emma Watson dons dress made of this (Met Gala 2016)"  #https://www.youtube.com/watch?v=t2j4hsQdMs0
+පහකටකලින්නවය = "Emma Watson interview: Gripping new drama, media scrutiny and playing Belle"  #https://www.youtube.com/watch?v=TH0WGanHJf8&t=5s
+පහකටකලින්දහය = "Emma Watson & Caitlin Moran - In Conversation for Our Shared Shelf" #https://www.youtube.com/watch?v=CynzW9Kz7Ds
+පහකටකලින්එකොළහ = "Emma Watson - One Young World 2016"  #https://www.youtube.com/watch?v=elbqER_ZrLQ
+පහකටකලින්දොළහ = "Emma Watson speech for HeForShe Second Year Anniversary"   #https://www.youtube.com/watch?v=mEjUcd33bCc
+පහකටකලින්දහතුන = "Emma Watson speech at United Nations - HeForShe IMPACT 10x10x10 University Parity Report"  #https://www.youtube.com/watch?v=_vzb7Io7oQ0
+පහකටකලින්දාහතර = "Emma Watson - Harper's Bazaar Women of the Year Awards 2016" #https://www.youtube.com/watch?v=XOKrdlZyF1Q
+පහකටකලින්පහළොව = "Emma Watson - Harpers Bazaar Women of the Year Awards 2016 speech"  #https://www.youtube.com/watch?v=mzPMIHAfUcw
+හතරකටකලින්එක = "Beauty and the Beast cast live chat on Facebook" #https://www.youtube.com/watch?v=TtlDoajvUvc
+හතරකටකලින්දෙක = "Emma Watson answers questions from kids | Entertainment Weekly" #https://www.youtube.com/watch?v=YxPD6R-OlQI
+හතරකටකලින්තුන් =  "Beauty and the Beast cast at Paris premiere"  #https://www.youtube.com/watch?v=ZAZqFgJ_tlw
+හතරකටකලින්හතර = "Beauty and the Beast: press conference with cast in Paris" #https://www.youtube.com/watch?v=bfcka4GQsUw
+හතරකටකලින්පහ = "Beauty and the Beast - Behind the scenes with Emma Watson" #https://www.youtube.com/watch?v=g9J1bRRxD_Q
+හතරකටකලින්හය = "Beauty and the Beast - Emma Watson Interview #1" #https://www.youtube.com/watch?v=POvGKnq1d60
+හතරකටකලින්හත = "Emma Watson at Los Angeles premiere of Beauty and the Beast" #https://www.youtube.com/watch?v=Exid3xOUzsY
+හතරකටකලින්අට = "Beauty and the Beast cast live Q&A on Facebook" #https://www.youtube.com/watch?v=4h_YllMlUkc
+හතරකටකලින්නවය = "Beauty and the Beast - Emma Watson Interview #2" #https://www.youtube.com/watch?v=MrBftDdA_aU
+හතරකටකලින්දහය = "Emma Watson live chat for EW on facebook"  #https://www.youtube.com/watch?v=dSMr0JN0w1A
+හතරකටකලින්එකොළහ = "Emma Watson and Dan Stevens dish on work and life | USA TODAY" #https://www.youtube.com/watch?v=bU7IzNB4LBQ
+හතරකටකලින්දොළහ ="The Circle - Emma Watson interview #1" #https://www.youtube.com/watch?v=YlOPNGnPPS4
+හතරකටකලින්දහතුන = "The Circle cast live chat on Twitter" #https://www.youtube.com/watch?v=4NISy3EFbNY
+හතරකටකලින්දාහතර = "Emma Watson live on facecbook for TF1" #https://www.youtube.com/watch?v=jhfP7VHMo6I
+තුනකටකලින්එක = "Emma Watson about anti-bullying and harassment" #https://www.youtube.com/watch?v=W4mlXlBH53Q
+තුනකටකලින්දෙක = "Emma Watson Interviews Rupi Kaur for Our Shared Shelf" #https://www.youtube.com/watch?v=mkw1S1eqNBI
+තුනකටකලින්තුන = "Emma Watson Interviews Reni Eddo Lodge" #https://www.youtube.com/watch?v=AwWCZl_OUsY
+තුනකටකලින්හතර =  "Emma Watson In Conversation with Dr Denis Mukwege Nobel Peace Price" #https://www.youtube.com/watch?v=o2MkmSCYs0I
+තුනකටකලින්පහ = "Emma Watson - In The Bag | British Vogue" #https://www.youtube.com/watch?v=NX2EYRAlYHw
+තුනකටකලින්හය = "Emma Watson Interviews Author Rebecca Solnit" #https://www.youtube.com/watch?v=qdNHZDq7XHQ
+තුනකටකලින්හත = "Emma Watson in Coversation with Steve Chbosky" #https://www.youtube.com/watch?v=dYDZWUmUdxI
+
+
+
+
+
+harry = "Harry Potter"
+beautyandthebeast = "Beauty And The Beast"
+MyWeekwithMarilyn = "My Week with Marilyn"
+TheBlingRing = "The Bling Ring"
+ThisIstheEnd = "This Is the End"
+Noah = "Noah"
+Colonia = "Colonia"
+Regression = "Regression"
+TheCircle = "The Circle"
+LittleWomen = "Little Women" 
+harry1 = "/_Philosophers_Stone"
+harry2 = "Harry Potter and the Chamber of Secrets"
+harry3 = "Harry Potter and the Prisoner of Azkaban"
+harry4 = "Harry Potter and the Goblet of Fire"
+harry5 = "Harry Potter and the Order of the Phoenix"
+harry6 = "Harry Potter and the Half-Blood Prince"
+harry7 = "Harry Potter and the Deathly Hallows – Part 1"
+harry8 = "Harry Potter and the Deathly Hallows – Part 2"
+
+phmode = "Photos Mode(Recomended)"
+urlmode = "Url Mode (HighSpeed Fowarding)"
+
+
+
+urlback = "🔙Back To Years List🔙"
+imgback = "🔙Years List🔙"
+backmain = "🔙🔙Back To Main Menu🔙🔙"
+
+
+firstbuttonset = [[KeyboardButton(Photos)], [KeyboardButton(Interviews)],[KeyboardButton(Tiktok_And_Others)],[KeyboardButton(Movies)],[KeyboardButton(Random_Photos_And_Videos)],[KeyboardButton(Watch_And_Download_All)]]
+
+
+modebutt = [[KeyboardButton(phmode)], [KeyboardButton(urlmode)]]
+
+
+yearsbuttonurl = [[KeyboardButton(2000)],[KeyboardButton(2002)],[KeyboardButton(2003)],[KeyboardButton(2004)],[KeyboardButton(2005)],[KeyboardButton(2006)],[KeyboardButton(2007)],[KeyboardButton(2008)],[KeyboardButton(2009)],[KeyboardButton(2010)],[KeyboardButton(2011)],[KeyboardButton(2012)],[KeyboardButton(2013)],[KeyboardButton(2014)],[KeyboardButton(2015)],[KeyboardButton(2016)],[KeyboardButton(2017)],[KeyboardButton(2018)],[KeyboardButton(2019)],[KeyboardButton(2020)],[KeyboardButton(2021)],[KeyboardButton(backmain)]]
+
+
+
+API_KEY = '2033518274:AAFYBZRILqiBBDB8_wFaCLSqFQe4mwmGAJs'
+
+
+
+
+updater = Updater(token="2033518274:AAFYBZRILqiBBDB8_wFaCLSqFQe4mwmGAJs")
+dispatcher = updater.dispatcher
+
+
+
+
+
