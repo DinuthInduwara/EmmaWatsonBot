1c7b1f8fbb924c1ba92a8e26b28cf6065ed3c62c
DinuthInduwara <89961332+DinuthInduwara@users.noreply.github.com>
2021-11-10 20:22:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,6 @@ logging.info('Starting Bot...')
     
 def messageHandler(update: Update, context: CallbackContext):
     
-    text = str(update.message.text).lower()
-    logging.info(f'User ({update.message.chat.id}) says: {text}')
-
-    # Bot response
-    response = responses.get_response(text)
-    update.message.reply_text(response)
-
 
     if "/start" in update.message.text:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my bot!", reply_markup=ReplyKeyboardMarkup(firstbuttonset))
